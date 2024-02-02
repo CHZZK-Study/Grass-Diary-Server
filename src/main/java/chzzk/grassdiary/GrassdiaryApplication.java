65a715dad58d43984b41cf5ddf08d5dfe82c8fcc
HongYeseul <ghd8119@naver.com>
2024-02-02 10:10:51 +0900

    chore: init

---
+++ src/main/java/chzzk/grassdiary/GrassdiaryApplication.java
@@ -0,0 +1,13 @@
+package chzzk.grassdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrassdiaryApplication.class, args);
+	}
+
+}
