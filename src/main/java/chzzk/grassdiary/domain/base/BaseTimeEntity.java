4e55df94c28aecd73f43bc09b3021fd5bcefd350
HongYeseul <ghd8119@naver.com>
2024-02-02 13:39:02 +0900

    :sparkles: feat(TimeEntity): BaseTimeEntity 추가 - BaseCreatedTimeEntity: 생성 날짜 필드가 추가되는 클래스 - BaseTimeEntity: 생성, 수정 날짜 필드가 추가되는 클래스

---
+++ src/main/java/chzzk/grassdiary/domain/base/BaseTimeEntity.java
@@ -0,0 +1,17 @@
+package chzzk.grassdiary.domain.base;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity extends BaseCreatedTimeEntity{
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
