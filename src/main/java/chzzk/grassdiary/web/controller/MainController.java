bfea620ac3123698ae3f935aea6f8cf8304c228f
HongYeseul <ghd8119@naver.com>
2024-02-16 15:57:20 +0900

    remove(controller): 필요없는 코드 삭제

--- src/main/java/chzzk/grassdiary/web/controller/MainController.java
+++ src/main/java/chzzk/grassdiary/web/controller/MainController.java
@@ -4,12 +4,10 @@ import chzzk.grassdiary.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("main")
 public class MainController {
     private final MainService mainService;
 
--- src/main/java/chzzk/grassdiary/web/controller/MyPageController.java
+++ src/main/java/chzzk/grassdiary/web/controller/MyPageController.java
@@ -5,12 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("myPage")
 public class MyPageController {
     private final MyPageService myPageService;
 
