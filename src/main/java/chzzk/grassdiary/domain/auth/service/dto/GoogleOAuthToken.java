ea665c5cf6c10fdc937b014ddbfbc7744bf6bab0
minisyu <iswater.kim@gmail.com>
2024-02-21 02:17:22 +0900

    feat(GoogleOAuthToken): 구글 서버로부터 받는 토큰 dto 작성

---
+++ src/main/java/chzzk/grassdiary/domain/auth/service/dto/GoogleOAuthToken.java
@@ -0,0 +1,14 @@
+package chzzk.grassdiary.domain.auth.service.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+// JSON 직렬화(object -> JSON) 및 역직렬화(JSON -> object) 시 사용되는 네이밍 전략 설정 어노테이션
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record GoogleOAuthToken(
+        String accessToken,
+        String refreshToken
+) {
+}
