edac71dcb3afecc631cefa664ecb08b537589bf7
minisyu <iswater.kim@gmail.com>
2024-02-21 14:11:42 +0900

    refactor(Member): auth 기능 구현으로 Member 클래스 수정
    - nickname 길이 지정 및 not null 지정
    - email 검증 및 not null 지정
    - @Table 어노테이션을 사용하여 unique key 지정
    - 필요한 속성만 선택적으로 설정하고 생성자 가독성 향상을 위해 @Builder 어노테이션 사용

--- src/main/java/chzzk/grassdiary/domain/member/Member.java
+++ src/main/java/chzzk/grassdiary/domain/member/Member.java
@@ -10,6 +10,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,20 +24,32 @@ import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter // DBTest에서 임시 사용
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "NICKNAME_EMAIL_UNIQUE",
+                columnNames = {"nickname", "email"}
+        )}
+) // 닉네임, 이메일 중복 금지
 public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-    private String nickname;
+    @Column(nullable = false)
+    @Size(min = 1, max = 20)
+    private String nickname;  // auth
 
-    private String email;
+    @Column(nullable = false, length = 200)
+    @Email(message = "유효하지 않은 이메일 주소입니다.")
+    private String email; // auth
 
     @Lob
-    private String profileImageUrl;
+    private String profileImageUrl; // auth
 
     private Long grassCount;
 
