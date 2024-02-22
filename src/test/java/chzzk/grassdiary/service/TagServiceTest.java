c4bbfe4e93f8fb4ed79c6c6f69380a2e1f4e291c
HongYeseul <ghd8119@naver.com>
2024-02-22 10:24:17 +0900

    test(TagService): 멤버의 전체 해시태그 검색

---
+++ src/test/java/chzzk/grassdiary/service/TagServiceTest.java
@@ -0,0 +1,106 @@
+package chzzk.grassdiary.service;
+
+import chzzk.grassdiary.domain.color.ConditionLevel;
+import chzzk.grassdiary.domain.diary.Diary;
+import chzzk.grassdiary.domain.diary.DiaryRepository;
+import chzzk.grassdiary.domain.diary.tag.DiaryTag;
+import chzzk.grassdiary.domain.diary.tag.MemberTags;
+import chzzk.grassdiary.domain.diary.tag.MemberTagsRepository;
+import chzzk.grassdiary.domain.diary.tag.TagList;
+import chzzk.grassdiary.domain.diary.tag.TagListRepository;
+import chzzk.grassdiary.domain.member.Member;
+import chzzk.grassdiary.domain.member.MemberRepository;
+import chzzk.grassdiary.web.dto.diary.TagDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TagServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    DiaryRepository diaryRepository;
+
+    @Autowired
+    MemberTagsRepository memberTagsRepository;
+    @Autowired
+    TagListRepository tagListRepository;
+
+    @Autowired
+    TagService tagService;
+
+    protected List<Member> members;
+    protected List<Diary> diaries;
+    protected List<MemberTags> memberTags;
+    protected List<TagList> tagLists;
+
+    @BeforeEach
+    void beforeEach() {
+        // 멤버 3명 만들기
+        members = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            members.add(Member.builder()
+                            .nickname("testMember" + i)
+                            .email("member" + i + "@test.com")
+                            .build());
+        }
+        members = memberRepository.saveAll(members);
+
+        // 15개 일기
+        diaries = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            diaries.add(Diary.builder()
+                            .member(members.get(i%3))
+                            .content("testContent" + i)
+                            .hasTag(true)
+                            .conditionLevel(ConditionLevel.LEVEL_5)
+                    .build());
+        }
+        diaries = diaryRepository.saveAll(diaries);
+
+        // 태그 리스트 생성
+        tagLists = new ArrayList<>();
+        tagLists.add(TagList.builder().tag("tag1").build());
+        tagLists.add(TagList.builder().tag("tag2").build());
+        tagLists.add(TagList.builder().tag("tag3").build());
+        tagLists = tagListRepository.saveAll(tagLists);
+
+        // 멤버 태그 생성
+        memberTags = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            memberTags.add(new MemberTags(members.get(i), tagLists.get(0)));
+            memberTags.add(new MemberTags(members.get(i), tagLists.get(1)));
+        }
+        memberTags.add(new MemberTags(members.get(2), tagLists.get(2)));
+        memberTags = memberTagsRepository.saveAll(memberTags);
+    }
+
+    @Test
+    @DisplayName("멤버별 해시태그 검색")
+    void searchHashTags() {
+        // Given
+        Member member = members.get(2); // 1번 멤버 선택
+        Long memberId = member.getId();
+
+        // When
+        List<TagDTO> dto = tagService.getMemberTags(memberId);
+
+        // Then
+        System.out.println("SIZE: " + dto.size());
+        for (int i = 0; i < dto.size(); i++) {
+            System.out.println("tagId: "+dto.get(i).tagId());
+            System.out.println("tag: "+dto.get(i).tag());
+        }
+        assertThat(dto.size()).isEqualTo(3);
+    }
+
+}
