cb4eb1c69897beccf486ff9e99710e425f4fd1c6
HongYeseul <ghd8119@naver.com>
2024-02-23 23:48:38 +0900

    fix(TagList): 잘못된 엔티티 구조 수정

--- src/main/java/chzzk/grassdiary/domain/diary/tag/TagList.java
+++ src/main/java/chzzk/grassdiary/domain/diary/tag/TagList.java
@@ -5,15 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Getter
 @Setter // DBTest에서 임시 사용
@@ -27,9 +22,6 @@ public class TagList {
 
     @Column(nullable = false)
     private String tag;
-
-    @OneToMany(mappedBy = "tagList")
-    private List<MemberTags> memberTags = new ArrayList<>();
 
     @Builder
     public TagList(String tag) {
