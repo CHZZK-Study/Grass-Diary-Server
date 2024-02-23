9e4d9636523422e10087eb1a832a4ffbe5e944ac
HongYeseul <ghd8119@naver.com>
2024-02-23 10:31:10 +0900

    feat(Member): rewardPoint 기본 값 0으로 설정

--- src/main/java/chzzk/grassdiary/domain/member/Member.java
+++ src/main/java/chzzk/grassdiary/domain/member/Member.java
@@ -35,6 +35,7 @@ public class Member extends BaseTimeEntity {
 
     private Long grassCount;
 
+    @ColumnDefault("0")
     private Integer rewardPoint;
 
     @ColumnDefault("false")
@@ -50,5 +51,6 @@ public class Member extends BaseTimeEntity {
         this.nickname = nickname;
         this.email = email;
         this.currentColorCode = currentColorCode;
+        this.rewardPoint = 0;
     }
 }
