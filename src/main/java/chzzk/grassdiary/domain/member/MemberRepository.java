15b16c917f993934368c9fe8c6d6258079b7f09d
minisyu <iswater.kim@gmail.com>
2024-02-25 21:03:38 +0900

    feat(MemberRepository): email을 통해 DB에서 Member 반환하는 기능 추가

--- src/main/java/chzzk/grassdiary/domain/member/MemberRepository.java
+++ src/main/java/chzzk/grassdiary/domain/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package chzzk.grassdiary.domain.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
