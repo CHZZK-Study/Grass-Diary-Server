c4d867b33645047fcd32498bf14ddf7825b99b8e
minisyu <iswater.kim@gmail.com>
2024-02-25 21:02:50 +0900

    refactor(OAuthClient): 로그인 및 회원가입 통합으로 메소드 파라미터 변경

--- src/main/java/chzzk/grassdiary/domain/auth/client/OAuthClient.java
+++ src/main/java/chzzk/grassdiary/domain/auth/client/OAuthClient.java
@@ -4,7 +4,7 @@ import chzzk.grassdiary.domain.auth.service.dto.GoogleOAuthToken;
 import chzzk.grassdiary.domain.auth.service.dto.GoogleUserInfo;
 
 public interface OAuthClient {
-    GoogleOAuthToken getGoogleAccessToken(String code, boolean isSignUp);
+    GoogleOAuthToken getGoogleAccessToken(String code);
 
     GoogleUserInfo getGoogleUserInfo(String accessToken);
 }
