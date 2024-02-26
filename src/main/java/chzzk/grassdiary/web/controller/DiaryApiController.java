c34b3407dd28f049a05906ab7d0511854797e23d
chjcoder <chjcoder@gmail.com>
2024-02-26 17:10:10 +0900

    rename : DiaryController -> DiaryApiController

---
+++ src/main/java/chzzk/grassdiary/web/controller/DiaryApiController.java
@@ -0,0 +1,34 @@
+package chzzk.grassdiary.web.controller;
+
+import chzzk.grassdiary.service.diary.DiaryService;
+import chzzk.grassdiary.web.dto.diary.DiaryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class DiaryApiController {
+
+    private final DiaryService diaryService;
+
+    @PostMapping("/api/diary")
+    public Long save(@RequestBody DiaryDto.Request requestDto) {
+        return diaryService.save(requestDto);
+    }
+
+    @PatchMapping("/api/diary/{id}")
+    public Long update(@PathVariable Long id, @RequestBody DiaryDto.Request requestDto) {
+        return diaryService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/diary/{id}")
+    public Long delete(@PathVariable Long id) {
+        diaryService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/diary/{id}")
+    public DiaryDto.Response findById(@PathVariable Long id) {
+        return diaryService.findById(id);
+    }
+}
