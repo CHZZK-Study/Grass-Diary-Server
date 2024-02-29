e308c103f6354786217f5da863364ba9425e1984
chjcoder <chjcoder@gmail.com>
2024-02-29 17:30:03 +0900

    fix : WebMvcConfigurer 수정
    .allowedOrigins("http://localhost:3000")로 변경

--- src/main/java/chzzk/grassdiary/config/WebMvcConfig.java
+++ src/main/java/chzzk/grassdiary/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
