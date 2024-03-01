450e21d53b36c7696d98d77bc151ed9aec94f15f
minisyu <iswater.kim@gmail.com>
2024-03-01 14:45:24 +0900

    refactor(JwtTokenProvider): 브라우저로부터 받은 JWT 액세스 토큰 검증 기능 구현

--- src/main/java/chzzk/grassdiary/domain/auth/jwt/JwtTokenProvider.java
+++ src/main/java/chzzk/grassdiary/domain/auth/jwt/JwtTokenProvider.java
@@ -1,27 +1,36 @@
 package chzzk.grassdiary.domain.auth.jwt;
 
+import chzzk.grassdiary.domain.auth.exception.AuthenticationException;
+import chzzk.grassdiary.domain.auth.exception.JwtException;
+import chzzk.grassdiary.domain.auth.service.dto.AuthMemberPayload;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-/**
- * 토큰 Payload에 저장될 정보
- * <p>
- * -> email, 생성일, 만료일
- */
 @Component
 public class JwtTokenProvider {
+    private static final String EXPIRED_ACCESS_TOKEN_MESSAGE = "EXPIRED_ACCESS_TOKEN";
+    private static final String MEMBER_ID = "id";
+    private static final String ALGORITHM = "HmacSHA256";
+
     @Value("${jwt.access.secret-key}")
     private String jwtAccessTokenSecretKey;
 
     @Value("${jwt.access.expiration}")
     private long jwtAccessTokenExpiration;
 
-    public String generateAccessTokenById(String socialId) {
+    public String generateAccessToken(AuthMemberPayload payload) {
         long currentDateTime = new Date().getTime();
         Date expiryDate = new Date(currentDateTime + jwtAccessTokenExpiration);
         SecretKeySpec secretKey = new SecretKeySpec(
@@ -29,21 +38,46 @@ public class JwtTokenProvider {
                 SignatureAlgorithm.HS256.getJcaName());
 
         return Jwts.builder()
-                .claim("id", socialId)
+                .claim(MEMBER_ID, payload.id())
                 .setExpiration(expiryDate)
                 .signWith(secretKey)
                 .compact();
     }
 
-    /**
-     * 스프링 서버는 브라우저로부터 받은 액세스 토큰으로부터 이메일을 추출하여 인증
-     */
+    public Long extractIdFromAccessToken(String token) {
+        validateAccessToken(token);
+        Jws<Claims> claimsJws = getAccessTokenParser().parseClaimsJws(token);
+        Long extractedId = claimsJws.getBody().get(MEMBER_ID, Long.class);
 
-    /**
-     * 액세스 토큰 검증 - interceptor
-     */
+        if (extractedId == null) {
+            String logMessage = "인증 실패(JWT 액세스 토큰 Payload id 누락) - 토큰 : " + token;
+            throw new AuthenticationException(logMessage);
+        }
+        return extractedId;
+    }
 
-    /**
-     * email 확인 메소드, 만료일 확인 메소드
-     */
+    private JwtParser getAccessTokenParser() {
+        return Jwts.parserBuilder()
+                .setSigningKey(new SecretKeySpec(
+                        jwtAccessTokenSecretKey.getBytes(StandardCharsets.UTF_8),
+                        ALGORITHM))
+                .build();
+    }
+
+    private void validateAccessToken(String token) {
+        try {
+            Claims claims = getAccessTokenParser().parseClaimsJws(token).getBody();
+        } catch (MalformedJwtException | UnsupportedJwtException exception) {
+            String logMessage = "인증 실패(잘못된 액세스 토큰인 경우): " + token;
+            throw new AuthenticationException(logMessage);
+        } catch (ExpiredJwtException expiredJwtException) {
+            throw new AuthenticationException("인증 실패(토큰 만료인 경우): " + EXPIRED_ACCESS_TOKEN_MESSAGE);
+        } catch (SignatureException signatureException) {
+            String logMessage = "인증 실패(잘못된 시그니처인 경우): " + token;
+            throw new AuthenticationException(logMessage);
+        } catch (JwtException jwtException) {
+            String logMessage = "인증 실패(기타 오류 발생): " + token;
+            throw new AuthenticationException(logMessage);
+        }
+    }
 }
