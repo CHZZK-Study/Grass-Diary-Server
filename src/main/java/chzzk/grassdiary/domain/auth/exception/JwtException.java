ef16b70cf376330ff9a4f9839aeea3c6acd36dbf
minisyu <iswater.kim@gmail.com>
2024-03-01 14:50:42 +0900

    feat(JWTException, AuthenticationException): Auth 관련 커스텀 예외 생성

---
+++ src/main/java/chzzk/grassdiary/domain/auth/exception/JwtException.java
@@ -0,0 +1,11 @@
+package chzzk.grassdiary.domain.auth.exception;
+
+public class JwtException extends RuntimeException {
+    public JwtException(String message) {
+        super(message);
+    }
+
+    public JwtException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
