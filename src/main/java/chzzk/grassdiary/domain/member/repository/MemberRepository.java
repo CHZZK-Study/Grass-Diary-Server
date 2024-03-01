b7426e74a72357c78b8b163a71189776f3844521
minisyu <iswater.kim@gmail.com>
2024-03-01 14:54:23 +0900

    feat(MemberRepository): 사용자 조회 기능 추가 및 패키지 이동

--- src/main/java/chzzk/grassdiary/domain/member/repository/MemberRepository.java
+++ src/main/java/chzzk/grassdiary/domain/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
-package chzzk.grassdiary.domain.member;
+package chzzk.grassdiary.domain.member.repository;
 
+import chzzk.grassdiary.domain.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findById(Long id);
 }
