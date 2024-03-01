0809f2d501fef6fb181e924e506f41bc81c129cc
minisyu <iswater.kim@gmail.com>
2024-03-01 15:13:57 +0900

    style: 주석 제거

--- src/main/java/chzzk/grassdiary/domain/auth/service/dto/GoogleUserInfo.java
+++ src/main/java/chzzk/grassdiary/domain/auth/service/dto/GoogleUserInfo.java
@@ -2,9 +2,6 @@ package chzzk.grassdiary.domain.auth.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * 구글 서버로부터 받은 사용자 정보
- */
 public record GoogleUserInfo(
         String id,
 
