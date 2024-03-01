6f6ca3fa8902796571e89250d572cb93ac83d44d
minisyu <iswater.kim@gmail.com>
2024-03-01 15:05:24 +0900

    refactor(GoogleOAuthToken): @JsonProperty 어노테이션으로 변경

--- src/main/java/chzzk/grassdiary/domain/auth/service/dto/GoogleOAuthToken.java
+++ src/main/java/chzzk/grassdiary/domain/auth/service/dto/GoogleOAuthToken.java
@@ -1,12 +1,9 @@
 package chzzk.grassdiary.domain.auth.service.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-// JSON 직렬화(object -> JSON) 및 역직렬화(JSON -> object) 시 사용되는 네이밍 전략 설정 어노테이션
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GoogleOAuthToken(
-        String accessToken,
-        String refreshToken
+        @JsonProperty("access_token")
+        String accessToken
 ) {
 }
