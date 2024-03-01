cff3b09754c7f47804a3167a665a3fc724329a3b
minisyu <iswater.kim@gmail.com>
2024-03-01 15:03:37 +0900

    test(JwtProviderTest): JWT 토큰 생성 및 검증 테스트 완료

--- src/test/java/chzzk/grassdiary/domain/auth/jwt/JwtTokenProviderTest.java
+++ src/test/java/chzzk/grassdiary/domain/auth/jwt/JwtTokenProviderTest.java
@@ -1,7 +1,9 @@
 package chzzk.grassdiary.domain.auth.jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+import chzzk.grassdiary.domain.auth.service.dto.AuthMemberPayload;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,15 +14,27 @@ class JwtTokenProviderTest {
     private JwtTokenProvider jwtTokenProvider;
 
     @Test
-    public void 액세스_토큰을_생성한다() {
+    public void JWT_액세스_토큰_생성() {
         // given
-        String id = "290940394";
+        AuthMemberPayload payload = AuthMemberPayload.from(1L);
 
         // when
-        String accessToken = jwtTokenProvider.generateAccessTokenById(id);
+        String accessToken = jwtTokenProvider.generateAccessToken(payload);
 
         // then
         System.out.println("accessToken = " + accessToken);
         assertThat(accessToken).isNotNull();
+    }
+
+    @Test
+    public void JWT_액세스_토큰_검증() {
+        // given
+        AuthMemberPayload payload = AuthMemberPayload.from(1L);
+        String accessToken = jwtTokenProvider.generateAccessToken(payload);
+
+        // when
+        // then
+        assertThatCode(() -> jwtTokenProvider.extractIdFromAccessToken(accessToken))
+                .doesNotThrowAnyException();
     }
 }
