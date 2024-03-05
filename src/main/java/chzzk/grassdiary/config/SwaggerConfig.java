ded4748ca7c5477963ba7d4f092b3c1e38459ace
minisyu <iswater.kim@gmail.com>
2024-03-06 00:06:11 +0900

    feat: Swagger 보안 설정 추가

--- src/main/java/chzzk/grassdiary/config/SwaggerConfig.java
+++ src/main/java/chzzk/grassdiary/config/SwaggerConfig.java
@@ -2,6 +2,14 @@ package chzzk.grassdiary.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -10,4 +18,17 @@ import org.springframework.context.annotation.Configuration;
                 version = "v1"))
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Collections.singletonList(securityRequirement));
+    }
 }
