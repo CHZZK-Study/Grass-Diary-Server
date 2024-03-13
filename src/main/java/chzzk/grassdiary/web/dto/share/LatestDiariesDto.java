c2e260b4fb726e3c6b5b993b4fb455e504d53ccb
minisyu <iswater.kim@gmail.com>
2024-03-13 21:33:09 +0900

    refactor: 공유 페이지 dto 수정

--- src/main/java/chzzk/grassdiary/web/dto/share/LatestDiariesDto.java
+++ src/main/java/chzzk/grassdiary/web/dto/share/LatestDiariesDto.java
@@ -1,4 +1,25 @@
 package chzzk.grassdiary.web.dto.share;
 
-public record LatestDiariesDto(Long diaryId, String content, int size, String nickname) {
+import chzzk.grassdiary.domain.diary.Diary;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public record LatestDiariesDto(
+        Long diaryId,
+        String content,
+        int diaryLikeCount,
+        String nickname,
+        String createdAt) {
+
+    public static List<LatestDiariesDto> of(List<Diary> diaries) {
+        return diaries.stream()
+                .map(d -> new LatestDiariesDto(
+                        d.getId(),
+                        d.getContent(),
+                        d.getDiaryLikes().size(),
+                        d.getMember().getNickname(),
+                        d.getCreatedAt()
+                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                )).toList();
+    }
 }
--- src/main/java/chzzk/grassdiary/web/dto/share/Top10DiariesDto.java
+++ src/main/java/chzzk/grassdiary/web/dto/share/Top10DiariesDto.java
@@ -1,8 +1,26 @@
 package chzzk.grassdiary.web.dto.share;
 
+import chzzk.grassdiary.domain.diary.Diary;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import org.springframework.data.domain.Page;
+
 public record Top10DiariesDto(
         Long diaryId,
         String diaryContent,
         int diaryLikeCount,
-        String nickname) {
+        String nickname,
+        String createdAt) {
+
+    public static List<Top10DiariesDto> of(Page<Diary> diaries) {
+        return diaries.stream()
+                .map(d -> new Top10DiariesDto(
+                        d.getId(),
+                        d.getContent(),
+                        d.getDiaryLikes().size(),
+                        d.getMember().getNickname(),
+                        d.getCreatedAt()
+                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                )).toList();
+    }
 }
