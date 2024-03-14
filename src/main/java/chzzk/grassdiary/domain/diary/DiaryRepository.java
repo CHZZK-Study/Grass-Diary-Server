a3b2dfd1717560e7873077fb10048a73072ed47b
minisyu <iswater.kim@gmail.com>
2024-03-15 01:03:46 +0900

    import문 추가

--- src/main/java/chzzk/grassdiary/domain/diary/DiaryRepository.java
+++ src/main/java/chzzk/grassdiary/domain/diary/DiaryRepository.java
@@ -3,6 +3,7 @@ package chzzk.grassdiary.domain.diary;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
