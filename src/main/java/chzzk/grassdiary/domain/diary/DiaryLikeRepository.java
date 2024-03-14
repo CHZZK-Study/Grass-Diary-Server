ea0ba82759500f3e4c85d33c65eaed423fb9cbaf
minisyu <iswater.kim@gmail.com>
2024-03-15 00:55:10 +0900

    rebase

--- src/main/java/chzzk/grassdiary/domain/diary/DiaryLikeRepository.java
+++ src/main/java/chzzk/grassdiary/domain/diary/DiaryLikeRepository.java
@@ -1,6 +1,9 @@
 package chzzk.grassdiary.domain.diary;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaryLikeRepository extends JpaRepository<DiaryLike, Long> {
+    Optional<DiaryLike> findByDiaryIdAndMemberId(Long diaryId, Long memberId);
+
 }
